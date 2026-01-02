ce47af3de79eee4b5ceee00b5b82de7a36f29b43
iamnaresh06 <06.nareshreddy@gmail.com>
2026-01-02 19:29:37 +0530

    Port Changing Fixing

--- scambank_project/settings.py
+++ scambank_project/settings.py
@@ -128,8 +128,9 @@ LOGOUT_REDIRECT_URL = 'home'
 
 # Email Configuration
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_TIMEOUT = 10  # Seconds
