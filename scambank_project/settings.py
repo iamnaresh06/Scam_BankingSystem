ad1fffcfbd75f7304754fc577de8fe785b2afcfd
iamnaresh06 <06.nareshreddy@gmail.com>
2026-01-02 14:13:55 +0530

    Removing Secret Key

--- scambank_project/settings.py
+++ scambank_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=3h^fzb^b!x_+6tx&*wo7iru$x8bqcoj2f@lmbi0%37i40kf-a'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
